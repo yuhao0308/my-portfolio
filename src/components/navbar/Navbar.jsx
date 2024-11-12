c09763c908056a590bd00849ee900810672f038d
yuhaowang <ks973111@gmail.com>
2024-11-11 22:52:34 -0500

    Fix asset paths and add GitHub Actions workflow

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
   <div className="navbar">
     <Sidebar/>
     <div className="wrapper">
-      <motion.span 
+      <motion.span
       initial={{opacity: 0, scale: 0.5}}
       animate={{opacity: 1, scale: 1}}
       transition={{duration: 0.5}}
       >YuHao Wang</motion.span>
       <div className="social">
-        <a href="#"><img src='/Linkedin.png' alt="Linkedin"/></a>
-        <a href="#"><img src='/Github.png' alt="Github"/></a>
+        <a href="#"><img src="./Linkedin.png" alt="Linkedin"/></a>
+        <a href="#"><img src="./Github.png" alt="Github"/></a>
       </div>
     </div>
   </div>);
